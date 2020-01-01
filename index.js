67a712c212eb7b0e4486e4343fdb70ed37f2b04f
Keith Andre Rose <andrerose722@gmail.com>
2020-01-01 13:39:08 -0500

    login methods

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const reddit = require("./reddit");
+const botUser = require('./env/botUser');
+
 (async () => {
-  await reddit.init("dankmemes");
-  let results = await reddit.getResults(35)
-  console.log(results)
+  await reddit.init();
+  await reddit.login(botUser.name, botUser.password)
+  // await reddit.makePost('dankmemes',{
+  //   type: 'text',
+  //   title: 'This is a automated text test',
+  //   text: 'Hello World'
+  // })
+  // let results = await reddit.getResults("dankmemes", 35)
   await reddit.shutdown()
   debugger
 })();
--- reddit.js
+++ reddit.js
@@ -1,6 +1,11 @@
 const puppeteer = require("puppeteer");
 
+const REDDIT_URL = `https://old.reddit.com/`;
 const SUBREDDIT_URL = subreddit => `https://old.reddit.com/r/${subreddit}/`;
+const SUBREDDIT_SUBMIT_LINK_URL = subreddit =>
+  `https://old.reddit.com/r/${subreddit}/submit`;
+const SUBREDDIT_SUBMIT_TEXT_URL = subreddit =>
+  `https://old.reddit.com/r/${subreddit}/submit?selftext=true`;
 
 const self = {
   browser: null,
@@ -11,31 +16,87 @@ const self = {
       headless: false
     });
     self.page = await self.browser.newPage();
+  },
+  login: async (user, passwd) => {
+    await self.page.goto(REDDIT_URL, {
+      waitUntil: "networkidle0"
+    });
+
+    await self.page.type('input[name="user"]', user, { delay: 30 });
+    await self.page.type('input[name="passwd"]', passwd, { delay: 30 });
+    await self.page.click("#login_login-main > div.submit > button");
+
+    await self.page.waitFor(
+      'form[action="https://old.reddit.com/logout"], div[class="status error"]'
+    );
+
+    let error = await self.page.$('div[class="status error"]');
+    if (error) {
+      let errorMsg = await (await error.getProperty("innerText")).jsonValue();
+      console.log(`User: ${user} failed to login`);
+      console.log(`Site Error: ${errorMsg}`);
+      setTimeout(() => {
+        console.log(`Shutting down in 5 seconds`);
+      }, 400);
+      setTimeout(async () => {
+        await self.shutdown();
+      }, 5400);
+    } else console.log(`[${user}] Login Successful`);
+  },
+  makePost: async (subreddit, data = {}) => {
+    switch (data.type) {
+      case "text":
+        await self.page.goto(SUBREDDIT_SUBMIT_TEXT_URL(subreddit), {
+          waitUntil: "networkidle0"
+        });
+
+        await self.page.waitFor(3000)
+        
+        await self.page.type('textarea[name="title"]', data.title);
+        await self.page.type('textarea[name="text"]', data.text);
+        break;
+      case "link":
+        await self.page.goto(SUBREDDIT_SUBMIT_LINK_URL(subreddit), {
+          waitUntil: "networkidle0"
+        });
+
+        await self.page.waitFor(3000)
+
+        await self.page.type('#url', data.url);
+        await self.page.type('textarea[name="title"]', data.title);
+        break;
+      default:
+        break;
+    }
+
+    await self.page.click('#newlink > div.spacer > button')
+
+  },
+  getResults: async (subreddit, desiredAmount) => {
+    let results = [];
     await self.page.goto(SUBREDDIT_URL(subreddit), {
       waitUntil: "networkidle0"
     });
-  },
-  getResults: async desiredAmount => {
-    let results = []
 
     do {
-      let pageResults = await self.parseResults() 
-      results = [...results, ...pageResults]
-      if( results.length < desiredAmount){
-        let nextPageButton = await self.page.$('span[class="next-button"] > a[rel="nofollow next"]');
-        if(nextPageButton){
-          await nextPageButton.click()
+      let pageResults = await self.parseResults();
+      results = [...results, ...pageResults];
+      if (results.length < desiredAmount) {
+        let nextPageButton = await self.page.$(
+          'span[class="next-button"] > a[rel="nofollow next"]'
+        );
+        if (nextPageButton) {
+          await nextPageButton.click();
           await self.page.waitForNavigation({
             waitUntil: "networkidle0"
-          })
+          });
         } else {
-          break
+          break;
         }
       }
+    } while (results.length <= desiredAmount);
 
-    } while (results.length <= desiredAmount)
-
-    return results.slice(0, desiredAmount)
+    return results.slice(0, desiredAmount);
   },
   parseResults: async () => {
     let elements = await self.page.$$('#siteTable > div[class*="thing"]');
@@ -78,11 +139,13 @@ const self = {
       };
       results.push(thing);
     }
-    return results
+    return results;
   },
   shutdown: async () => {
-    await self.browser.close()
-    self.browser.disconnect()
+    console.log(`Shutting down`)
+    await self.browser.close();
+    self.browser.disconnect();
+    process.exit(1);
   }
 };
 
